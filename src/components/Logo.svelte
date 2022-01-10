05a1a94ecd599cfd6a800f2e6d5a664304215ee6
Andrew Lee <candrewlee14@gmail.com>
2022-01-10 03:30:45 -0700

    add scrambling letter animation

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,11 +1,33 @@
 <script>
-    import Typewriter from 'svelte-typewriter'
+    import Typewriter from 'svelte-typewriter';
+
+    import { onMount } from 'svelte';
+    let lockedIn = false;
+    let fadeIn = false;
 </script>
 
 <div>
-    <Typewriter scramble={700} scrambleSlowdown={true} delay={50}><span>AND</span></Typewriter>
-    <Typewriter scramble={700} scrambleSlowdown={true} delay={100}><span>REW</span></Typewriter>
-    <Typewriter scramble={900} scrambleSlowdown={true} delay={150}><span>LEE</span></Typewriter>
+    {#if !lockedIn}
+    <Typewriter scramble={700} scrambleSlowdown={true} delay={50}>
+        <span class={lockedIn ? "locked" : "unlocked"}>AND</span>
+    </Typewriter>
+    <Typewriter scramble={700} scrambleSlowdown={true} delay={100}>
+        <span class={lockedIn ? "locked" : "unlocked"}>REW</span>
+    </Typewriter>
+    <Typewriter scramble={900} scrambleSlowdown={true} delay={150} on:done={() => {
+        lockedIn = true; 
+        setTimeout(() => {
+            fadeIn = true
+        }, 100);
+    }}>
+        <span class={lockedIn ? "locked" : "unlocked"}>LEE</span>
+    </Typewriter>
+    {/if}
+    {#if lockedIn}
+        <span class={fadeIn ? "locked" : "unlocked"}>AND</span>
+        <span class={fadeIn ? "locked" : "unlocked"}>REW</span>
+        <span class={fadeIn ? "locked" : "unlocked"}>LEE</span>
+    {/if}
 </div>
 
 <style>
@@ -15,6 +37,7 @@
         margin: 0;
         font: 1rem;
         line-height: 1rem;
+        transition: color 2s;
     }
     div {
         border: 3px solid;
@@ -23,4 +46,11 @@
         min-height: 62px;
         @apply p-1 m-6 drop-shadow-md hover_drop-shadow-xl;
     }
+    .locked {
+        @apply text-black;
+    }
+    .unlocked {
+        @apply text-gray-500;
+    }
+
 </style>
