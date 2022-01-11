3e82da32c170b408ba7586cc125649c87f496727
Andrew Lee <candrewlee14@gmail.com>
2022-01-10 20:54:52 -0700

    fix dark mode icon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,20 @@
 <script>
 	import { onMount } from 'svelte';
+import { get_all_dirty_from_scope } from 'svelte/internal';
 
 
 	import '../app.css';
 	import Logo from '../components/Logo.svelte';
 	import Navbar from '../components/Navbar.svelte';
 
+	let theme = null;
 	function updateMode() {
 		if (localStorage.theme === 'dark') {
+			theme = 'dark';
 			document.body.classList.add('dark')
 		} else {
 			document.body.classList.remove('dark')
+			theme = 'light';
 		}
 	}
 
@@ -29,7 +33,14 @@
 <div class="flex flex-col w-full items-center" style="min-height: 100vh;">
 	<Logo />
 	<Navbar />
-	<button class="absolute transition-color t-1 l-1 m-1 text-black hover_text-gray-600 dark_text-white dark_hover_text-gray-300 text-2xl" on:click={toggle}>✸</button>
+	<button class="absolute transition-color t-1 l-1 m-1 text-black hover_text-gray-600 dark_text-white dark_hover_text-gray-300 text-3xl" on:click={toggle}>
+	{#if theme === 'dark'}
+	✸
+	{/if}
+	{#if !(theme === 'dark')}
+	☾
+	{/if}
+	</button>
 	<div class="w-5/6 bg-lightgray dark_bg-gray-900 pb-1 mb-4" style="content: ' ';" />
 	<slot />
 </div>
